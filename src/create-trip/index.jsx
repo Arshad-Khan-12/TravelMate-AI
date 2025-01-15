4eb2e1480507dd03a606a294e8f558308073f619
Arshad-Khan-12 <ak4566335@gmail.com>
2025-01-15 17:15:02 +0530

    Auth and added gemini ai functionalities

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,14 +1,35 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { SelectBudgetOptions, SelectTravelsList } from "@/constants/options";
+import {
+  AI_PROMPT,
+  SelectBudgetOptions,
+  SelectTravelsList,
+} from "@/constants/options";
+
+// import { chatSession } from "@google/generative-ai";
+// import chatSession from "@google_generative-ai";
+import { chatSession } from "@/service/AIModel";
+
 import React, { useEffect, useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { toast } from "sonner";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 
 function CreateTrip() {
   const [place, setPlace] = useState();
   const [formData, setFormData] = useState({});
   const [selectedBudget, setSelectedBudget] = useState(null);
   const [selectedTravelType, setSelectedTravelType] = useState(null);
+  const [openDialog, setOpenDialog] = useState(false);
 
   const handleInputChange = (name, value) => {
     setFormData({ ...formData, [name]: value });
@@ -18,30 +39,68 @@ function CreateTrip() {
     console.log(formData);
   }, [formData]);
 
-  const onGenerateTrip = () => {
-    if (Object.keys(formData).length === 0) {
-      alert("Please fill all the fields");
+  const onGenerateTrip = async () => {
+    const user = localStorage.getItem("user");
+    if (user == null) {
+      // toast("Please login to generate trip");
+      setOpenDialog(true);
       return;
-    } else {
-      if (
-        !formData.destination ||
-        !formData.days ||
-        !formData.budget ||
-        !formData.travelType
-      ) {
-        alert("Please fill all the fields");
-        return;
-      } else if (formData.days <= 0) {
-        alert("Please enter a valid number of days");
-        return;
-      } else if (formData.days > 5 || formData.days < 2) {
-        alert("Please enter a number of days between 2 and 5");
-        return;
-      } else {
-        console.log("Generating Trip", formData);
-        // Call API to generate
-      }
     }
+
+    if (
+      !formData?.destination ||
+      !formData?.days ||
+      !formData?.budget ||
+      !formData?.travelType
+    ) {
+      toast("Please fill all the fields");
+      return;
+    } else if (formData?.days <= 0) {
+      toast("Please enter a valid number of days");
+      return;
+    } else if (formData?.days > 5 || formData?.days < 2) {
+      toast("Please enter a number of days between 2 and 5");
+      return;
+    }
+    const FINAL_PROMPT = AI_PROMPT.replace(
+      "{location}",
+      formData?.destination?.label
+    )
+      .replace("{totalDays}", formData?.days)
+      .replace("{traveler}", formData?.travelType)
+      .replace("{budget}", formData?.budget)
+      .replace("{totalDays}", formData?.days);
+
+    console.log(FINAL_PROMPT);
+
+    const result = await chatSession.sendMessage(FINAL_PROMPT);
+
+    console.log(result?.response?.text());
+  };
+
+  const login = useGoogleLogin({
+    onSuccess: (response) => getUserProfile(response),
+    onError: (error) => console.log(error),
+  });
+
+  const getUserProfile = async (tokenInfo) => {
+    axios
+      .get(
+        `https://www.googleapis.com/oauth2/v1/userinfo?accesstoken=${tokenInfo?.access_token}`,
+        {
+          headers: {
+            Authorization: `Bearer ${tokenInfo?.access_token}`,
+            Accept: "Application/json",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        localStorage.setItem("user", JSON.stringify(res.data));
+        setOpenDialog(false);
+        onGenerateTrip();
+        toast("Login Successful");
+      });
   };
 
   return (
@@ -121,6 +180,33 @@ function CreateTrip() {
       <div className="flex items-end justify-end m-10">
         <Button onClick={onGenerateTrip}>Generate Trip</Button>
       </div>
+
+      <Dialog open={openDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>
+              <img
+                className="h-24 mx-auto"
+                src="public/TravelMate AI.png"
+                alt=""
+              />
+            </DialogTitle>
+            <DialogDescription>
+              <h2 className="font-bold text-xl text-gray-800 text-center  ">
+                Sign in with Google
+              </h2>
+              <p className="mt-5">
+                {" "}
+                Sign in with Google Authentication securely to continue using
+                the App{" "}
+              </p>
+              <Button onClick={login} className="w-full mt-5" variant="">
+                Sign in
+              </Button>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
